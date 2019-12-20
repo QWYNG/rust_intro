b6026b12f533d10c6aa10f3ea4d1017e7795ed80
qwyng <ikusawasi@gmail.com>
2019-12-20 19:43:13 +0900

    trait separate

--- ch08/ex08/examples/trait_basics.rs
+++ ch08/ex08/examples/trait_basics.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone)]
 struct CartesianCoord {
     x: f64,
     y: f64,
@@ -6,6 +7,22 @@ struct CartesianCoord {
 struct PolarCoord {
     r: f64,
     theta: f64,
+}
+
+struct Matrix([[f64; 2]; 2]);
+
+trait LinearTransform: Coordinates {
+    fn transform(self, matrix: &Matrix) -> Self;
+
+    fn rotate(self, theta: f64) -> Self
+    where
+        Self: Sized,
+    {
+        self.transform(&Matrix([
+            [theta.cos(), -theta.sin()],
+            [theta.sin(), -theta.cos()],
+        ]))
+    }
 }
 
 trait Coordinates {
@@ -20,6 +37,35 @@ impl Coordinates for CartesianCoord {
 
     fn from_cartesian(cart: CartesianCoord) -> Self {
         cart
+    }
+}
+
+impl LinearTransform for CartesianCoord {
+    fn transform(mut self, matrix: &Matrix) -> Self {
+        let x = self.x;
+        let y = self.y;
+        let m = matrix.0;
+
+        self.x = m[0][0] * x + m[0][1] * y;
+        self.y = m[1][0] * x + m[1][1] * y;
+        self
+    }
+}
+
+impl LinearTransform for PolarCoord {
+    fn transform(mut self, matrix: &Matrix) -> Self {
+        let x = self.x;
+        let y = self.y;
+        let m = matrix.0;
+
+        self.x = m[0][0] * x + m[0][1] * y;
+        self.y = m[1][0] * x + m[1][1] * y;
+        self
+    }
+
+    fn rotate(mut self, theta: f64) -> Self {
+        self.theta += theta;
+        self
     }
 }
 
@@ -61,7 +107,9 @@ fn main() {
     let p = PolarCoord::from_cartesian(c);
     println!("r = {}, θ = {}", p.r, p.theta);
 
-    print_point(p)
+    print_point(p);
+    let p = (1.0, 0.0).to_cartesian();
+    print_point(p.rotate(std::f64::consts::PI))
 }
 
 fn print_point(point: impl Coordinates) {
